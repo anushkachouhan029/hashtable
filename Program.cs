8c5a425c52fb358c31cfebb03f66ac93fc139b7b
anushkachouhan029 <anushkachouhan029>
2022-12-11 12:11:41 +0530

    added uc3 paranoid or not

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace DataStructure_HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Data Structure Hash Table Program");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(19);
+
+            hash.Add(0, "Paranoids");
+            hash.Add(1, "are");
+            hash.Add(2, "not");
+            hash.Add(3, "paranoid");
+            hash.Add(4, "because");
+            hash.Add(5, "they");
+            hash.Add(6, "are");
+            hash.Add(7, "paranoid");
+            hash.Add(8, "but");
+            hash.Add(9, "because");
+            hash.Add(10, "they");
+            hash.Add(11, "keep");
+            hash.Add(12, "putting");
+            hash.Add(13, "themselves");
+            hash.Add(14, "deliberately");
+            hash.Add(15, "into");
+            hash.Add(16, "paranoid");
+            hash.Add(17, "avoidable");
+            hash.Add(18, "situations");
+
+            for (int i = 0; i < hash.size; i++)
+            {
+                Console.WriteLine($"{i} index value : {hash.Get(i)}");
+            }
+
+            Console.WriteLine("\nRemoving a Word from Paragraph ==>");
+            hash.Remove(17);
+            for (int i = 0; i < hash.size; i++)
+            {
+                Console.WriteLine($"{i} index value : {hash.Get(i)}");
+            }
+
+        }
+    }
+}
